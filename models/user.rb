555b78225792684495aad1cad5d2cd3a9959fa3b
Shell <shellfarley@gmail.com>
2021-06-12 14:25:15 +1000

    user page update

--- models/user.rb
+++ models/user.rb
@@ -1,1 +1,9 @@
 require 'bcrypt' 
+
+
+def create_user(first_name, surname, email, password)
+  password_digest = BCrypt::Password.create(password)
+  params = [first_name, surname, email, password_digest]
+  sql_query = "INSERT INTO users(first_name, surname, email, password_digest) VALUES($1, $2, $3, $4)"
+  run_sql(sql_query, params)
+end
