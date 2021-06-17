2e309dc6cc577220affb9b3394ad3762ab3e271d
Shell <shellfarley@gmail.com>
2021-06-17 14:39:20 +1000

    js update

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -1,6 +1,6 @@
 console.log("hello")
 
-let like_click = document.querySelectorAll('#heart')
+let like_click = document.querySelectorAll('button#heart')
 like_click.addEventListener('click', background_change) 
 
 function background_change(){
