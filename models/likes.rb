b9547d4321f719a281741a20f7a963935a56ed26
Shell <shellfarley@gmail.com>
2021-06-19 12:35:27 +1000

    README update

--- models/likes.rb
+++ models/likes.rb
@@ -1,6 +1,5 @@
 
 def create_like(user_id, podcast_id)
-  # params = [ user_id, podcast_id ]
   run_sql("INSERT INTO podcast_likes(user_id, podcast_id) VALUES($1, $2);", [user_id, podcast_id])
 end
 
